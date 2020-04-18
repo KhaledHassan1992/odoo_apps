2adb2c6bce9a1164c41f0ba791e8fe316213f9cd
KhaledHassan1992 <khaled.abdel-monem@ibnsina-pharma.com>
2020-04-18 13:20:56 +0200

    _product_by_supplier

---
+++ product_by_supplier/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Product Group by Supplier",
+    "summary": "Product Group by Supplier",
+    "description": """
+        you can listing all products by supplier.
+    """,
+    "version": "12.0",
+    "author": "Khaled Hassan",
+    "website": "https://www.linkedin.com/in/khaled-hassan-222600173/",
+    "category": "Product",
+    "depends": ['product'],
+    "data": [
+        'views/product_views.xml',
+    ],
+    "license": "LGPL-3",
+    "images": ['static/description/group_by_supplier.jpg', 'static/description/supplier_group.jpg'],
+}
