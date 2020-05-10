f1d9c5a09eca39b0729515920911d396b11d6b7a
KhaledHassan1992 <khaled.abdel-monem@ibnsina-pharma.com>
2020-05-10 16:48:09 +0200

    max_quantity

---
+++ so_max_quantity_allowed/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "so_max_quantity_allowed",
+
+    'summary': """sale order product maximum quantity allowed""",
+
+    'description': """allow product maximum quantity in product template 
+    and and make warning if ypu exceed maximum quantity""",
+
+    'author': "KhaledHassan",
+    'website': 'https://www.linkedin.com/in/khaled-hassan-222600173/',
+    'category': 'sale',
+    'version': '0.1',
+    'depends': ['base', 'product', 'sale'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'images': ['static/description/*.jpg', ],
+}
