ac4022a1e2150bcf7efbd12d29843b37f583842c
khaled.abdel-monem <khaled.abdel-monem@digi360.tech>
2021-09-12 15:56:33 +0200

    maintenance_date

---
+++ maintenance_date/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'maintenance date',
+    'version': '10.0',
+    'category': 'Assets',
+    'description': """
+    """,
+    'author': "Ibnsina",
+    'depends': [
+        'base_accounting_kit',
+        'account',
+    ],
+    'data': [
+        'views/maintenance_date.xml',
+        'data/sequence.xml',
+    ],
+}
