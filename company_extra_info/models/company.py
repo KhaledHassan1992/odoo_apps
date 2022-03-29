b188261ee951d793de1eec351c08a56e570b920f
KhaledHassan1992 <khaled.cs2017@gmail.com>
2022-03-29 12:03:03 +0200

    delete_unwanted_apps

--- company_extra_info/models/company.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError
-
-
-class company(models.Model):
-    _inherit = 'res.company'
-    fax = fields.Char('Fax')
-    extra_info = fields.Text('Extra Information')
-
